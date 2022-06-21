c58f11fb3bfafab75dbdbf98e3c2821e72467fc4
Lundberg <kevin.lundberg@sweco.se>
2022-06-21 20:29:24 +0200

    improved message logic, added rooms

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
 import { GiDogHouse } from "react-icons/gi";
 import { FaUserAstronaut } from "react-icons/fa";
@@ -36,6 +36,7 @@ import {
   CheckboxGroup,
   InputGroup,
   InputLeftElement,
+  InputRightElement,
 } from "@chakra-ui/react";
 
 import { io } from "socket.io-client";
@@ -43,38 +44,110 @@ import { io } from "socket.io-client";
 let socket;
 
 function App() {
+  const { isOpen, onToggle, onClose, onOpen } = useDisclosure();
+
   const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState("");
+
   const [clientID, setClientID] = useState("");
+  const [clients, setClients] = useState([]);
+
   const [randomColor, setRandomColor] = useState("");
+  const [avatar, setAvatar] = useState("");
+
+  const [currentRoom, setCurrentRoom] = useState("");
+  const [rooms, setRooms] = useState([]);
+
+  const scrollRef = useRef(null);
+
+  console.log(currentRoom);
+  console.log(rooms);
+
+  useEffect(() => {
+    scrollRef.current.scrollIntoView({
+      behavior: "smooth",
+      // block: "end",
+      // inline: "nearest",
+    });
+  }, [messages]);
 
   useEffect(() => {
     socket = io("http://localhost:4000");
 
-    socket.on("connect", () => {
-      console.log("Ansluten till servern");
+    socket.on("connection", () => {
+      console.log("Connected to server");
+    });
+
+    socket.on("joined_room", (data) => {
+      console.log(data);
+      setRooms(data);
+    });
+
+    socket.on("added_room", (data) => {
+      setRooms(data);
+      console.log(rooms);
+    });
+
+    socket.on("current_room", (data) => {
+      setCurrentRoom(data.name);
+    });
+
+    socket.on("joined_room", (data) => {
+      console.log(`${data} has joined the room`);
     });
 
     socket.on("message", (data) => {
       console.log(data);
     });
 
-    socket.on("clientID", (data) => {
-      setClientID(data);
-      const random = Math.floor(Math.random() * 16777215).toString(16);
-      console.log(random);
-      setRandomColor(random);
+    socket.on("getAllClients", (data) => {
+      console.log(data);
+      setClients(data);
     });
 
-    socket.on("chatMessage", ({ message, clientID, randomColor }) => {
-      console.log(message);
-      console.log(clientID);
-      console.log(randomColor);
+    socket.on("clientID", (data) => {
+      console.log(data);
+      setClientID(data);
+      const random = Math.floor(Math.random() * 16777215).toString(16);
+      setRandomColor(random);
+      setAvatar(
+        `https://avatars.dicebear.com/api/pixel-art-neutral/${data}.svg`
+      );
+    });
 
+    // socket.on(
+    //   "chatMessage",
+    //   ({ message, clientID, randomColor, avatar, currentRoom }) => {
+    //     console.log(message, clientID, randomColor, avatar, currentRoom);
+    //     // setMessages((prevMessage) => {
+    //     //   return [
+    //     //     ...prevMessage,
+    //     //     { message, clientID, randomColor, avatar, currentRoom },
+    //     //   ];
+    //     // });
+    //   }
+    // );
+
+    socket.on("chatMessage2", (data) => {
+      console.log("get chat");
+      console.log(data);
       setMessages((prevMessage) => {
-        return [...prevMessage, { message, clientID, randomColor }];
+        return [
+          ...prevMessage,
+          {
+            message: data.message,
+            clientID: data.clientID,
+            randomColor: data.randomColor,
+            avatar: data.avatar,
+            currentRoom: data.currentRoom,
+          },
+        ];
       });
-      console.log(messages);
+    });
+
+    socket.on("disconnect", () => {
+      setRooms([]);
+      setMessages([]);
     });
 
     // socket.on("new_client", (data) => {
@@ -84,8 +157,29 @@ function App() {
     return () => socket.off();
   }, []);
 
+  console.log(messages);
+  console.log(currentRoom);
+  console.log(rooms);
+
   const handleMessage = (message) => {
-    socket.emit("chatMessage", { message, clientID, randomColor });
+    socket.emit("chatMessage", {
+      message,
+      clientID,
+      randomColor,
+      avatar,
+      currentRoom,
+    });
+  };
+
+  const addRoom = (roomName) => {
+    console.log("addRoom");
+    console.log(roomName);
+    socket.emit("add_room", roomName);
+  };
+
+  const joinRoom = (roomName) => {
+    console.log(roomName);
+    socket.emit("join_room", roomName);
   };
 
   return (
@@ -113,12 +207,12 @@ function App() {
               <Flex justifyContent="space-between" alignItems="center" gap={2}>
                 <Text fontSize="2xl" fontWeight="bold" color="green.400">
                   {clientID
-                    ? `Welcome ${clientID.slice(0, 10)}`
+                    ? `Welcome ${clientID.slice(0, 10)}...`
                     : "Waiting for server..."}
                 </Text>
                 <Button
                   colorScheme="purple"
-                  alignSelf="flex-end"
+                  alignSelf="flex-start"
                   width="min-content"
                   size="sm"
                 >
@@ -155,18 +249,52 @@ function App() {
                 >
                   Rooms
                 </Button>
-                <Text textDecor="underline">Room 1</Text>
-                <Text>Room 2</Text>
                 <Button
                   // isDisabled
                   m={0}
                   p={0}
                   bg="none"
                   fontSize="1xl"
-                  leftIcon={<IoAddOutline />}
+                  // leftIcon={<IoAddOutline />}
+                  onClick={() => onToggle()}
                 >
                   Add room
                 </Button>
+                <Collapse in={isOpen}>
+                  <InputGroup>
+                    <InputRightElement
+                      children={<IoAddOutline />}
+                      onClick={() => {
+                        addRoom(currentRoom);
+                        // setRoom("");
+                      }}
+                    />
+                    <Input
+                      variant="outline"
+                      maxW="150px"
+                      // bg="gray.100"
+                      // color="blackAlpha.900"
+                      value={currentRoom}
+                      fontSize="lg"
+                      onChange={(e) => setCurrentRoom(e.target.value)}
+                      onKeyPress={(e) => {
+                        if (e.key === "Enter") {
+                          joinRoom(currentRoom);
+                          // setRoom("");
+                        }
+                      }}
+                    />
+                  </InputGroup>
+                  {rooms &&
+                    rooms.reverse().map((room) => (
+                      <Text
+                        bgColor={currentRoom === room.name && "green"}
+                        // onClick={() => joinRoom(room.name)}
+                      >
+                        {room.name}
+                      </Text>
+                    ))}
+                </Collapse>
               </Flex>
               <Flex flexDir="column" p={1} m={1}>
                 <Button
@@ -180,9 +308,10 @@ function App() {
                 >
                   Users
                 </Button>
-                <Text>User 1</Text>
-                <Text>User 2</Text>
-                <Text>User 3</Text>
+                {/* {clients &&
+                  clients
+                    .reverse()
+                    .map((client) => <Text>{client.slice(0, 5)}</Text>)} */}
               </Flex>
             </Flex>
           </GridItem>
@@ -193,6 +322,7 @@ function App() {
             borderRight="2px solid black"
             borderRadius={10}
             p={2}
+
             // my={2}
             // minH="100%"
           >
@@ -213,7 +343,8 @@ function App() {
                 {messages &&
                   messages.map((message) => (
                     <Flex
-                      flexDir="column"
+                      flexDir="row"
+                      justifyContent="space-between"
                       border="1px solid purple"
                       bgColor={`#${message.randomColor}`}
                       bgGradient="revert"
@@ -221,13 +352,19 @@ function App() {
                       p={1}
                       overflowWrap="break-word"
                     >
-                      <Text fontSize="10px">{message.clientID}</Text>
-                      <Text color="white">{message.message}</Text>
+                      <Text>{message.currentRoom}</Text>
+                      <Flex flexDir="column">
+                        <Text fontSize="10px">{message.clientID}</Text>
+                        <Text color="white">{message.message}</Text>
+                      </Flex>
+                      <Image src={message.avatar} boxSize={10} rounded="full" />
                     </Flex>
                   ))}
+                <Box ref={scrollRef}></Box>
                 <InputGroup>
                   <InputLeftElement children={<BsChatFill />} />
                   <Input
+                    placeholder="Write message..."
                     variant="outline"
                     bg="gray.100"
                     value={message}
@@ -242,6 +379,7 @@ function App() {
                   />
                 </InputGroup>
                 <Button
+                  minH={10}
                   colorScheme="green"
                   onClick={() => {
                     handleMessage(message);
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const { Server } = require("socket.io");
+const { v4 } = require("uuid");
 
 const io = new Server(4000, {
   /* options */
@@ -8,25 +9,107 @@ const io = new Server(4000, {
   },
 });
 
+// Flyttas till databas sen
+let clients = [];
+let rooms = [];
+
 io.on("connection", (socket) => {
+  // Emit when connected (single client)
+  socket.emit("connection");
+  // Client with ID has connected
   console.log(`Socket with ID: ${socket.id} has connected`);
   //   Welcome message (single client)
   socket.emit("message", "Welcome to Social fabric");
-  socket.emit("clientID", socket.id);
+
   // Broadcast when a user connects (to all clients, expect the one connecting)
   socket.broadcast.emit("message", "A user has joined the chat");
-  // To all clients in general - io.emit //
+  // [To all clients in general - io.emit]
+
+  // CLIENT LOGIC
+  // Send ID for current Client
+
+  console.log(clients);
+
+  // Emit client ID (single client)
+  socket.emit("clientID", socket.id);
+  // Emit clients to all clients
+  clients.push(socket.id);
+  io.emit("getAllClients", clients);
+
+  // ROOM LOGIC
+  socket.on("add_room", (data) => {
+    const newRoom = {
+      // id: v4(),
+      name: data,
+      messages: [],
+    };
+    rooms.push(newRoom);
+
+    // Vad gör socket.join egentligen?
+    socket.join(data);
+
+    socket.emit("current_room", newRoom);
+
+    io.to(data).emit("added_room", rooms);
+  });
+
+  socket.on("join_room", (data) => {
+    // data: string med rumnamnet
+    console.log(`${socket.id} has joined ${data}`);
+
+    // Gå med i ett rum
+    socket.join(data);
+
+    // Berättar för alla i rummet som lyssnar på "joined_room"
+    // att socketen med detta id't har gått med
+    io.to(data).emit("joined_room", { id: socket.id, room: data });
+
+    // Skriver ut rummen socketen är med i
+    console.log(socket.rooms);
+  });
+
+  // MESSAGE LOGIC
+  //   Send message test
+  socket.on("chatMessage", (data) => {
+    console.log(rooms);
+
+    console.log(data);
+
+    rooms.map((room) => {
+      if (room.name === data.currentRoom) {
+        const newMessage = {
+          message: data.message,
+          clientID: data.clientID,
+          randomColor: data.randomColor,
+          avatar: data.avatar,
+          currentRoom: data.currentRoom,
+        };
+        room.messages.push(newMessage);
+        data.messages = room.messages;
+      }
+    });
+
+    console.log(data.currentRoom);
+
+    io.to(data.currentRoom).emit("chatMessage2", data);
+
+    // console.log(avatar);
+    // io.emit("chatMessage", {
+    //   message,
+    //   clientID,
+    //   randomColor,
+    //   avatar,
+    //   currentRoom,
+    // });
+
+    // socket.join();
+
+    // io.emit("id", socket.id);
+  });
 
   //   This runs when clients disconnects
   socket.on("disconnect", (reason) => {
     console.log(`Socket ${socket.id} disconnected. Reason ${reason}`);
     io.emit("message", "A user has left the chat");
-  });
-
-  //   Send message test
-  socket.on("chatMessage", ({ message, clientID, randomColor }) => {
-    // console.log(`${socket.id} has sent message: ${data}`);
-    io.emit("chatMessage", { message, clientID, randomColor });
-    io.emit("id", socket.id);
   });
 });
